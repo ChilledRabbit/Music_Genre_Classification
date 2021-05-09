d9b95babff91510c5b47715f70def74b94ea0b94
Ben <benjinxed@yahoo.co.uk>
2021-05-09 21:49:39 +0100

    Initial Commit

---
+++ genre_classifier.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+
+def load_data(dataset_path):
+    with open(dataset_path, "r") as fp:
+        data = json.load(fp)
+
+    # Convert lists into numpy arrays
+
+
+# PLAN
+# load data
+# Split the data into the train and test sets
+# Build the network architecture
+# Compile network
+# Train network
+
