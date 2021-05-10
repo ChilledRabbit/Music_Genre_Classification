de9c882aa2f5f9bb4f98543b390b246a8e1678b2
Ben <benjinxed@yahoo.co.uk>
2021-05-10 14:57:22 +0100

    Added genre_classifier.py

--- genre_classifier.py
+++ genre_classifier.py
@@ -1,17 +1,62 @@
 import json
 import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+
+DATASET_PATH = "data.json"
+
 
 def load_data(dataset_path):
     with open(dataset_path, "r") as fp:
         data = json.load(fp)
 
     # Convert lists into numpy arrays
+    inputs = np.array(data["mfcc"])
+    targets = np.array(data["labels"])
+
+    return intputs, targets
 
 
-# PLAN
-# load data
-# Split the data into the train and test sets
-# Build the network architecture
-# Compile network
-# Train network
+if __name__ == "__main__":
 
+    # Load data
+    inputs, targets = load_data(DATASET_PATH)
+
+    # Split the data into the train and test sets
+
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs,
+                                                                              targets,
+                                                                              test_size=0.3)
+    # Build the network architecture
+
+    model = keras.Sequential([
+        # Input layer
+        keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
+
+        # 1st hidden layer
+        keras.layers.Dense(512, activation="relu"),
+        # 2nd hidden layer
+        keras.layers.Dense(512, activation="relu"),
+        # 3rd hidden layer
+        keras.layers.Dense(512, activation="relu"),
+
+        # Output layer
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    # Compile network
+
+    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(optimizer=optimizer,
+                  loss="sparse_categorical_crossentropy",
+                  metrics=["accuracy"]
+                  )
+    model.summary()
+
+    # Train network
+
+    model.fit(inputs_train,
+              targets_train,
+              validation_data=(inputs_test, targets_test),
+              epochs=50,
+              batch_size=32)
