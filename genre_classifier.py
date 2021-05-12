521e05bbf7fd5db53d6770ef8bec3e59cc7f68e8
Ben <benjinxed@yahoo.co.uk>
2021-05-12 14:59:32 +0100

    Implemented measures to solve overfitting

--- genre_classifier.py
+++ genre_classifier.py
@@ -2,9 +2,11 @@ import json
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 DATASET_PATH = "data.json"
-
+print(tf.__version__)
 
 def load_data(dataset_path):
     with open(dataset_path, "r") as fp:
@@ -14,7 +16,28 @@ def load_data(dataset_path):
     inputs = np.array(data["mfcc"])
     targets = np.array(data["labels"])
 
-    return intputs, targets
+    return inputs, targets
+
+def plot_history(history):
+
+    fig, axs = plt.subplots(2)
+
+    # Create accuracy subplot
+
+    axs[0].plot(history.history["accuracy"], label="Train Accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="Test Accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    axs[1].plot(history.history["loss"], label="Train Error")
+    axs[1].plot(history.history["val_loss"], label="Test Error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -34,11 +57,14 @@ if __name__ == "__main__":
         keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
 
         # 1st hidden layer
-        keras.layers.Dense(512, activation="relu"),
+        keras.layers.Dense(512, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),  # Dropout
         # 2nd hidden layer
-        keras.layers.Dense(512, activation="relu"),
+        keras.layers.Dense(512, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),  # Dropout
         # 3rd hidden layer
-        keras.layers.Dense(512, activation="relu"),
+        keras.layers.Dense(512, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),  # Dropout
 
         # Output layer
         keras.layers.Dense(10, activation="softmax")
@@ -55,8 +81,12 @@ if __name__ == "__main__":
 
     # Train network
 
-    model.fit(inputs_train,
-              targets_train,
-              validation_data=(inputs_test, targets_test),
-              epochs=50,
-              batch_size=32)
+    history = model.fit(inputs_train,
+                        targets_train,
+                        validation_data=(inputs_test, targets_test),
+                        epochs=50,
+                        batch_size=32)
+
+    # Plot accuracy and error over the epochs
+
+    plot_history(history)
